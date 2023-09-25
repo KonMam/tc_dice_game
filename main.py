59fc1ff2e1e726c7413257111e13afa5ab0dc5b6
Konstantinas Mamonas <k.mamonas@gmail.com>
2023-09-25 20:05:22 +0300

    initial commit, Dice and DiceRoller classes

---
+++ main.py
@@ -0,0 +1,46 @@
+from random import random
+from typing import List
+
+
+class Dice:
+    def __init__(self, sides) -> None:
+        if sides in range(1, 100):
+            self.sides = sides
+        else:
+            raise ValueError("Number of sides should be between 1 and 100")
+
+    def roll(self):
+        return random.randint(1, self.sides)
+
+    def __repr__(self) -> str:
+        return f"Dice({self.sides})"
+
+    def __str__(self) -> str:
+        return f"A {self.sides}-sided dice."
+
+
+class DiceRoller:
+    def __init__(self, dice_list: List[Dice]) -> None:
+        self.dice_list = dice_list
+        self.last_100_rolls = []
+
+    def roll(self):
+        for dice in self.dice_list:
+            roll = random.randint(1, dice.sides)
+            
+
+
+if __name__ == "__main__":
+    dice = Dice(6)
+    print(dice)
+
+
+
+# TODO: 
+# Save and read last_100_rolls from a file
+# Add support for 1 to 100 dice
+# Multiple dice throws support
+# Documentation
+# Unit Tests
+# Web API
+# Support for weighted Dice
